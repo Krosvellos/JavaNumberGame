22f3dc31de62242191c831ebd18d7ec342aa53f6
Krosvellos <daniel.abraham@seznam.cz>
2024-03-24 22:38:41 +0100

    Added basic logic. The game is now functional and runs in a while loop. Next step is to make the game more complex.

--- src/Guesser.java
+++ src/Guesser.java
@@ -7,9 +7,21 @@ public class Guesser {
     public void Guess(){
 
         Generator generate = new Generator();
+        while(true) {
         generate.generate();
 
-        System.out.println(generate.randNum);
+        System.out.println("Guess a number from 1 to 10:");
+        int userGuess = scanner.nextInt();
+
+            if (userGuess == generate.randNum) {
+                System.out.println("You guessed correct! The number is " + generate.randNum);
+
+            } else {
+                System.out.println("You lost! The number was " + generate.randNum);
+
+            }
+
+        }
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a number guessing game");
+        System.out.println("This is a number guessing game!");
         Guesser guessgame = new Guesser();
         guessgame.Guess();
     }
