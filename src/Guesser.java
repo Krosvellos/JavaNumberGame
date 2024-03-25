0d31de0ee4d62992c9f015b283e73c29b55e64a7
Krosvellos <daniel.abraham@seznam.cz>
2024-03-25 17:03:39 +0100

    Added more functionalities. The game now allows more trials. The user is given more chances with a hint on which number to try next.

--- src/Guesser.java
+++ src/Guesser.java
@@ -4,25 +4,36 @@ public class Guesser {
 
     Scanner scanner = new Scanner(System.in);
 
-    public void Guess(){
+    public void Guess() {
+        while (true) {
+            System.out.println();
+            System.out.println("A number is chosen from 1 to 10! Guess the number within 3 trials!");
 
-        Generator generate = new Generator();
-        while(true) {
-        generate.generate();
+            Generator generate = new Generator();
 
-        System.out.println("Guess a number from 1 to 10:");
-        int userGuess = scanner.nextInt();
+            generate.generate();
+            int k = 3;
 
-            if (userGuess == generate.randNum) {
-                System.out.println("You guessed correct! The number is " + generate.randNum);
+            int i;
+            for (i = 0; i < k; i++) {
+                System.out.println("Guess a number from 1 to 10:");
+                int userGuess = scanner.nextInt();
 
-            } else {
-                System.out.println("You lost! The number was " + generate.randNum);
+                if (userGuess == generate.randNum) {
+                    System.out.println("You guessed correct! The number was " + generate.randNum);
+                    break;
+                } else if (generate.randNum > userGuess && i != k - 1) {
+                    System.out.println("The number is higher then " + userGuess + " ! Try again!");
+                } else if (generate.randNum < userGuess && i != k - 1) {
+                    System.out.println("The number is lower than " + userGuess + " ! Try again!");
+                }
 
+            }
+            if (i == k) {
+                System.out.println("You are out of trails! The number was " + generate.randNum);
             }
 
         }
 
     }
-
 }
